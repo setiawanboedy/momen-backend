b6185da8280043b83886d04c3a0b8680a94e8231
setiawanboedy <gonisetiawan0@gmail.com>
2022-06-28 16:32:33 +0800

    secret

--- auth/auth_service.go
+++ auth/auth_service.go
@@ -3,6 +3,8 @@ package auth
 import (
 	"errors"
 	"momen/utils"
+	"strconv"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
@@ -23,8 +25,19 @@ func NewService() *jwtService {
 
 func (s *jwtService) GenerateToken(userID int) (string, error) {
 	_,dbConfig := utils.DatabaseSettings()
+
+
+	tokenHourLifespanString := "24"
+
+	tokenHourLifespan, err := strconv.Atoi(tokenHourLifespanString)
+
+	if err != nil {
+		return "", err
+	}
+
 	claim := jwt.MapClaims{}
 	claim["user_id"] = userID
+	claim["exp"] = time.Now().Add(time.Hour * time.Duration(tokenHourLifespan))
 	
 
 	// token header
