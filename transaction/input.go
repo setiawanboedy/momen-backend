84a157ffc623fada77001651fb9999736c33ba23
setiawanboedy <gonisetiawan0@gmail.com>
2022-06-29 18:35:55 +0800

    secret

--- transaction/input.go
+++ transaction/input.go
@@ -2,7 +2,6 @@ package transaction
 
 type TransactionInput struct {
 	UserID      int
-	Name        string `json:"name" binding:"required"`
 	Description string `json:"description" binding:"required"`
 	Category    string `json:"category" binding:"required"`
 	Amount      int    `json:"amount" binding:"required"`
